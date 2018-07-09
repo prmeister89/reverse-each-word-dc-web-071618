65113cd7b05d132a38c224c9a9910c56222507dd
Peter Meister <prmeister89@gmail.com>
2018-07-09 17:02:34 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(sentence)
+  array = sentence.split
+  new_array = []
+  
+  array.collect do |word|
+    new_array << word.reverse
+  end
+
+  return new_array.join(" ")
+end
